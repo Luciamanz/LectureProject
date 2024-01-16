945872e5fc024fbde35e24147d1df85cb2fb6f2b
luciamanz <lucia@Lucias-MacBook.local>
2024-01-16 14:32:39 -0500

    Added static file image, also to settings

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,3 +118,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
