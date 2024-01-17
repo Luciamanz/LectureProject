e3c113dc1252533009507888b4feb2eaf9932534
luciamanz <lucia@Lucias-MacBook.local>
2024-01-17 11:50:39 -0500

    Add Currency model

--- djangoProject/myapp/models.py
+++ djangoProject/myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Currency(models.Model):
+    iso = models.CharField(max_length=3)
+    long_name = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return f'{self.iso} {self.long_name}'
+
