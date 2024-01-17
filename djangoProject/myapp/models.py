515eeb93dc783c3759cb10e5af766f1e6762558b
luciamanz <lucia@Lucias-MacBook.local>
2024-01-17 12:10:39 -0500

    Added holding and rate models

--- djangoProject/myapp/models.py
+++ djangoProject/myapp/models.py
@@ -9,3 +9,31 @@ class Currency(models.Model):
     def __str__(self):
         return f'{self.iso} {self.long_name}'
 
+
+class Holdings(models.Model):
+    currency = models.ForeignKey(
+        Currency,
+    on_delete=models.CASCADE,
+    )
+    value = models.FloatField(default=0.0)
+    buy_date = models.DateField()
+#when related Currency deleted, delete this holding
+    def __str__(self):
+        return f'{self.currency.iso} {self.value} {self.buy_date}'
+
+class Rate(models.Model):
+    currency_one = models.ForeignKey(
+        Currency,
+        on_delete=models.CASCADE,
+        related_name='currency_one_set',
+    )
+    currency_two = models.ForeignKey(
+        Currency,
+        on_delete=models.CASCADE,
+        related_name='currency_two_set',
+    )
+    rate = models.FloatField(default = 1.0)
+    last_update_time=models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.currency_one} {self.currency_two} {self.rate} {self.last_update_time}'
